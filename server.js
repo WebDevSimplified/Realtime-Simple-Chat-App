993d7f3778b9cc85e6adb244552565b6019b8fbe
AirplaneGobrr <mottlcompound@gmail.com>
2021-09-29 12:13:59 -0700

    😎 You can now host the server

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
-const io = require('socket.io')(3000)
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Will run the server on port 3000
+// If you change the port make sure to change it in index.html and public/script.js
+// If you plan to deply the server, you will need to change the files public/script.js and index.html to use your public IP
+server.listen(3000, () => { 
+  console.log('listening on port 3000');
+});
 
 const users = {}
 
+// Socket has has not been changed
 io.on('connection', socket => {
   socket.on('new-user', name => {
     users[socket.id] = name
-    socket.broadcast.emit('user-connected', name)
+    socket.broadcast.emit('user-connected', name);
   })
   socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] });
   })
   socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
+    socket.broadcast.emit('user-disconnected', users[socket.id]);
     delete users[socket.id]
   })
 })
