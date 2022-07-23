3c96793b8d57fb90c9e34d51208796441960f571
ZainabIftikhar <zainabiftikhar87@gmail.com>
2022-07-23 15:00:27 -0400

    base app

--- server.js
+++ server.js
@@ -22,13 +22,13 @@ const users = {}
 io.on('connection', socket => {
   socket.on('new-user', name => {
     users[socket.id] = name
-    socket.broadcast.emit('user-connected', name);
+    //socket.broadcast.emit('user-connected', name);
   })
   socket.on('send-chat-message', message => {
     socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] });
   })
   socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id]);
+    //socket.broadcast.emit('user-disconnected', users[socket.id]);
     delete users[socket.id]
   })
 })
