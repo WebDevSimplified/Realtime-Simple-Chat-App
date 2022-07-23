e51d625394b7cbb9608239a2be6f570add650d3b
ZainabIftikhar <zainabiftikhar87@gmail.com>
2022-07-23 18:10:19 -0400

    is-typing implemented

--- server.js
+++ server.js
@@ -24,11 +24,17 @@ io.on('connection', socket => {
     users[socket.id] = name
     //socket.broadcast.emit('user-connected', name);
   })
+  
   socket.on('send-chat-message', message => {
     socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] });
   })
+  
   socket.on('disconnect', () => {
     //socket.broadcast.emit('user-disconnected', users[socket.id]);
     delete users[socket.id]
+  })
+  
+  socket.on('typing', name => {
+    socket.broadcast.emit('typing', name)
   })
 })
