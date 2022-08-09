5c4eeb339b071674dc509bfd346328de8b5600ed
ZainabIftikhar <zainabiftikhar87@gmail.com>
2022-08-09 12:16:00 -0400

    add script file

--- public/script.js
+++ public/script.js
@@ -7,7 +7,7 @@ const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 const typing = document.getElementById('typing')
-var indicator_case = 0
+var indicator_case = 1
 var live_message = ''
 
 const name = prompt('What is your name?')
