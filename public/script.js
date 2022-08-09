5ed0eccceb70d1879a04ac48f80351a4c279b7b4
ZainabIftikhar <zainabiftikhar87@gmail.com>
2022-08-09 11:51:14 -0400

    Is typing (#2)
    * fix css for is-typing
    
    * is-typing implemented
    
    * Branches description
    
    * update gitignore - DS_Store added
    
    * update npm packages
    
    * make data passing consisten (#1)
    
    -use one wrapper
    
    * Update script.js
    
    * pass empty parameter
    
    * Delete .DS_Store

--- public/script.js
+++ public/script.js
@@ -1,13 +1,20 @@
 const socket = io('http://localhost:3000')
+
+//StackOverflow code following youtube tutorial
+//Reference: https://stackoverflow.com/questions/16766488/socket-io-how-to-check-if-user-is-typing-and-broadcast-it-to-other-users
+
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
+const typing = document.getElementById('typing')
+var live_message = ''
 
 const name = prompt('What is your name?')
 appendMessage('You joined')
 socket.emit('new-user', name)
 
 socket.on('chat-message', data => {
+  typing.innerHTML = ""
   appendMessage(`${data.name}: ${data.message}`)
 })
 
@@ -19,12 +26,20 @@ socket.on('user-disconnected', name => {
   appendMessage(`${name} disconnected`)
 })
 
+socket.on('typing', data => {
+  typing.innerHTML = '<p>' + data.name + ' is typing...</p>'
+})
+
 messageForm.addEventListener('submit', e => {
   e.preventDefault()
   const message = messageInput.value
   appendMessage(`You: ${message}`)
   socket.emit('send-chat-message', message)
   messageInput.value = ''
+})
+
+messageInput.addEventListener('keypress', function(){
+  socket.emit('typing', '')
 })
 
 function appendMessage(message) {
