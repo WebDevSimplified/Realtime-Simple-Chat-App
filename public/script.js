f03a501f7f12f5d5271862d64be0dbaad13be92a
ZainabIftikhar <zainabiftikhar87@gmail.com>
2022-08-09 12:07:46 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,12 +2,22 @@ const socket = io('http://localhost:3000')
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
+<<<<<<< Updated upstream
+=======
+const typing = document.getElementById('typing')
+var indicator_case = 0
+var live_message = ''
+>>>>>>> Stashed changes
 
 const name = prompt('What is your name?')
 appendMessage('You joined')
 socket.emit('new-user', name)
 
 socket.on('chat-message', data => {
+<<<<<<< Updated upstream
+=======
+  typing.innerHTML = ''
+>>>>>>> Stashed changes
   appendMessage(`${data.name}: ${data.message}`)
 })
 
@@ -19,6 +29,15 @@ socket.on('user-disconnected', name => {
   appendMessage(`${name} disconnected`)
 })
 
+<<<<<<< Updated upstream
+=======
+socket.on('typing', data => {
+  if (indicator_case == 1){
+    typing.innerHTML = '<p>' + data.name + ' is typing...</p>'
+  }
+})
+
+>>>>>>> Stashed changes
 messageForm.addEventListener('submit', e => {
   e.preventDefault()
   const message = messageInput.value
